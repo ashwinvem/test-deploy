76548739fd3390c66edd5ea86a36cb61df9c9d31
Sunil Kashyap <sunil@innocirc.com>
2020-05-21 13:03:04 +0530

    Updates

---
+++ precache-manifest.25727654e36956c87893c9545f0f547a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29365890f61f33b6212c1fdedd903bea",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "96bef58bc6a465aaf45c",
+    "url": "/test-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a4adf17ce55e0404889",
+    "url": "/test-deploy/static/js/2.06d9ee34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy/static/js/2.06d9ee34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bef58bc6a465aaf45c",
+    "url": "/test-deploy/static/js/main.b9a72106.chunk.js"
+  },
+  {
+    "revision": "42bfe82b977c2e9c67b9",
+    "url": "/test-deploy/static/js/runtime-main.140dc620.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
