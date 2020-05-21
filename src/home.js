c4fa392d8606b61e0c9b054b8b09afd988186b41
Sunil Kashyap <sunil@innocirc.com>
2020-05-21 13:14:04 +0530

    page

--- src/home.js
+++ src/home.js
@@ -5,6 +5,7 @@ export default class Home extends Component {
         return (
             <div>
                 <h1>helo</h1>
+                <h2>again</h2>
             </div>
         )
     }
